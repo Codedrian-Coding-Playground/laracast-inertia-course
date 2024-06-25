e8b10ffe2f672b856ee6a3ecacbf7a2d8632b16f
Adrian Gaile Singh <adriangailee@gmail.com>
2024-06-26 00:58:12 +0800

    chore: Add CreateUserRequest for user registration validation

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,18 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests;
 
-Route::get('/', [UserController::class, 'index'])->name('index');
+Route::get('/', function () {
+    sleep(1);
+    return inertia('Home', [
+        'isActive' => 'dashboard'
+    ]);
+})->name('dashboard');
 Route::get('/profile', function () {
-    return inertia('Profile');
-});
+    return inertia('Profile', [
+        'isActive' => 'profile'
+    ]);
+})->name('profile');
+Route::inertia('/register', 'Auth/Register');
+Route::post('/register', [UserController::class, 'create'])->middleware([HandlePrecognitiveRequests::class]);;
