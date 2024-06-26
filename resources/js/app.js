00aa32245187acad77cd3431cc2cdd75ed4522ab
Adrian Gaile Singh <adriangailee@gmail.com>
2024-06-26 20:04:10 +0800

    chore: Update app.js to use ZiggyVue for global usage

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,7 +11,7 @@ createInertiaApp({
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
         let page = pages[`./Pages/${name}.vue`]
-        if (name !== 'Auth/Register') {
+        if ((name !== 'Auth/Register') && (name !== 'Auth/Login')) {
             page.default.layout = page.default.layout || Layout
         }
             return page;
@@ -19,7 +19,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
+            .use(ZiggyVue) /*I put ZiggyVue here so that I dont need to import ZiggyVue in every page*/
             .mount(el)
     },
     progress: {
