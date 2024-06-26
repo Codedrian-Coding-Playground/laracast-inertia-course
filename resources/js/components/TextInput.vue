55b5a95f8982a7ff047d16cacb5e4845521b4a81
Adrian Gaile Singh <adriangailee@gmail.com>
2024-06-26 20:03:46 +0800

    chore: Update TextInput component to bind to parent and add required props

--- resources/js/components/TextInput.vue
+++ resources/js/components/TextInput.vue
@@ -1,12 +1,37 @@
 <script setup>
-defineProps({
-    message: String
+/*To bind to the parent*/
+const model = defineModel({
+    type: String,
+    required: true,
 })
+defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    id: {
+        type: String,
+        required: true,
+    },
+    placeholder: {
+        type: String,
+        required: true,
+    }
+});
 </script>
 
 <template>
-<p>{{ message }}</p>
-    <slot></slot>
+    <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">{{ name }}</label>
+        <input
+            v-model="model"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline" :id="id" :type="type"
+            :placeholder="placeholder">
+    </div>
 </template>
 
 <style scoped>
