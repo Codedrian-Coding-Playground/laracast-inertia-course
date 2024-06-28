bcb1a892c7377cc5a6ecac7ebfc736c02c1eae7b
Adrian Gaile Singh <adriangailee@gmail.com>
2024-06-28 23:23:25 +0800

    Feature: Add upload profile feature

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,7 +1,7 @@
 <script setup>
 import TextInput from '../../components/TextInput.vue';
 import {Head, Link} from "@inertiajs/vue3";
-import { useForm } from 'laravel-precognition-vue-inertia';
+import {useForm} from 'laravel-precognition-vue-inertia';
 
 const form = useForm('post', '/login', {
     email: '',
@@ -18,27 +18,25 @@ const submit = () => form.submit({
 <template>
     <Head title="| Login"></Head>
     <div class="w-full max-w-xs mx-auto mt-9">
-        {{ form.remember_me}}
         <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" @submit.prevent="submit">
             <TextInput
-                v-model="form.email"
-                name="Email"
-                type="email"
                 id="email"
-                placeholder="John Doe"
+                v-model="form.email"
                 is_valid="form.invalid('email')"
+                name="Email"
+                placeholder="John Doe"
+                type="email"
                 @change="form.validate('email')"
             />
             <div v-if="form.invalid('email')" class="text-red-500">
                 <small>{{ form.errors.email }}</small>
             </div>
-
             <TextInput
+                id="password"
                 v-model="form.password"
                 name="Password"
-                type="password"
-                id="password"
                 placeholder="******************"
+                type="password"
                 @change="form.validate('password')"
             />
             <div v-if="form.invalid('password')" class="text-red-500">
@@ -46,18 +44,19 @@ const submit = () => form.submit({
             </div>
             <div class="flex flex-col items-center justify-between">
                 <div class="flex  justify-start w-full gap-2">
-                    <input type="checkbox" name="remember_me" v-model="form.remember_me">
+                    <input v-model="form.remember_me" name="remember_me" type="checkbox">
                     <label for="remember_me">Remember me</label>
                 </div>
 
-                <button type="submit"
+                <button :class="{'bg-gray-500 cursor-not-allowed': form.processing }"
                         :disabled="form.processing"
-                        :class="{'bg-gray-500 cursor-not-allowed': form.processing }"
-                        class="w-full mb-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                        class="w-full mb-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit">
                     Sign In
                 </button>
                 <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                    Don't have an account? <Link :href="route('register')" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Register here</Link>
+                    Don't have an account?
+                    <Link :href="route('register')" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Register here</Link>
                 </p>
             </div>
         </form>
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
     Route::get('/', function () {
-        sleep(1);
-
         return inertia('Home', [
             'isActive' => 'dashboard',
         ]);
