1fdd5a54b285d3799fdffb6434e87ae2d42adff8
Adrian Gaile Singh <adriangailee@gmail.com>
2024-07-04 23:41:08 +0800

    feat: Add soft delete functionality to User model

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -36,6 +36,11 @@ watch(
         500,
     ),
 );
+
+/*Delete*/
+function deleteUser(id) {
+    router.delete("users/" + id);
+}
 </script>
 
 <template>
@@ -58,108 +63,114 @@ watch(
             >Logout
         </Link>
     </div>
-    <form class="w-full flex justify-end p-5">
-        <label
-            class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-            for="default-search"
-            >Search</label
-        >
-        <div class="relative w-2/6">
-            <div
-                class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"
+    <div v-if="users.data.length > 0">
+        <form class="w-full flex justify-end p-5">
+            <label
+                class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+                for="default-search"
+                >Search</label
             >
-                <svg
-                    aria-hidden="true"
-                    class="w-4 h-4 text-gray-500 dark:text-gray-400"
-                    fill="none"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
+            <div class="relative w-2/6">
+                <div
+                    class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"
                 >
-                    <path
-                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                    />
-                </svg>
-            </div>
-            <input
-                id="default-search"
-                v-model="search"
-                class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search User..."
-                required
-                type="search"
-            />
-            <button
-                class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                type="submit"
-            >
-                Search
-            </button>
-        </div>
-    </form>
-    <div
-        class="w-3/5 mx-auto relative overflow-x-auto shadow-md sm:rounded-lg flex flex-col"
-    >
-        <table
-            class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
-        >
-            <thead
-                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
-            >
-                <tr>
-                    <th class="px-6 py-3" scope="col">Photo</th>
-                    <th class="px-6 py-3" scope="col">Name</th>
-                    <th class="px-6 py-3" scope="col">Email</th>
-                    <th class="px-6 py-3" scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="user in users.data" :key="user.id">
-                    <td class="px-6 py-4">
-                        <img
-                            :src="
-                                user.avatar_path ??
-                                'storage/avatars/default_photo.jpg'
-                            "
-                            alt="Profile photo of {{ user.firstName }}"
-                            class="max-w-20 h-20 rounded"
+                    <svg
+                        aria-hidden="true"
+                        class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                        fill="none"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
                         />
-                    </td>
-                    <td class="px-6 py-4 font-medium whitespace-nowrap">
-                        {{ user.firstName + " " + user.lastName }}
-                    </td>
-                    <td class="px-6 py-4">{{ user.email }}</td>
-                    <td class="px-6 py-4">
-                        <a
-                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                            href="#"
-                            >Edit</a
-                        >
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <!--    Pagination link-->
-    <div class="flex justify-between mt-5">
-        <div class="ml-10">
-            <Link
-                v-for="link in users.links"
-                :key="link.label"
-                :class="{ 'bg-blue-400': link.active }"
-                :href="link.url"
-                class="mx-1 px-1 rounded"
-                v-html="link.label"
+                    </svg>
+                </div>
+                <input
+                    id="default-search"
+                    v-model="search"
+                    class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Search User..."
+                    required
+                    type="search"
+                />
+                <button
+                    class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    type="submit"
+                >
+                    Search
+                </button>
+            </div>
+        </form>
+        <div
+            class="w-3/5 mx-auto relative overflow-x-auto shadow-md sm:rounded-lg flex flex-col"
+        >
+            <table
+                class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
             >
-            </Link>
+                <thead
+                    class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+                >
+                    <tr>
+                        <th class="px-6 py-3" scope="col">Photo</th>
+                        <th class="px-6 py-3" scope="col">Name</th>
+                        <th class="px-6 py-3" scope="col">Email</th>
+                        <th class="px-6 py-3" scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="user in users.data" :key="user.id">
+                        <td class="px-6 py-4">
+                            <img
+                                :src="
+                                    user.avatar_path ??
+                                    'storage/avatars/default_photo.jpg'
+                                "
+                                alt="Profile photo of {{ user.firstName }}"
+                                class="max-w-20 h-20 rounded"
+                            />
+                        </td>
+                        <td class="px-6 py-4 font-medium whitespace-nowrap">
+                            {{ user.firstName + " " + user.lastName }}
+                        </td>
+                        <td class="px-6 py-4">{{ user.email }}</td>
+                        <td class="px-6 py-4">
+                            <button
+                                class="font-medium text-white bg-red-600 hover:bg-red-800 p-1 rounded"
+                                @click="deleteUser(user.id)"
+                            >
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-        <p class="justify-self-end mr-10">
-            Showing {{ users.from }} to {{ users.to }} of
-            {{ users.total }} results
-        </p>
+        <!--    Pagination link-->
+        <div class="flex justify-between mt-5">
+            <div class="ml-10">
+                <Link
+                    v-for="link in users.links"
+                    :key="link.label"
+                    :class="{ 'bg-blue-400': link.active }"
+                    :href="link.url"
+                    class="mx-1 px-1 rounded"
+                    v-html="link.label"
+                >
+                </Link>
+            </div>
+            <p class="justify-self-end mr-10">
+                Showing {{ users.from }} to {{ users.to }} of
+                {{ users.total }} results
+            </p>
+        </div>
+    </div>
+    <div v-else>
+        <p class="mx-auto w-fit text-2xl">No record found</p>
     </div>
 </template>
 <style scoped></style>
