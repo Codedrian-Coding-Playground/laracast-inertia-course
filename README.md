ffbce6ae04f9d03660b962fdb38eba83d7801612
Adrian Gaile Singh <adriangailee@gmail.com>
2024-07-04 19:43:50 +0800

    Modify README

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # laracast-inertia-course
-- I learned about [Default Layout](https://inertiajs.com/pages#default-layouts:~:text=%3C/script%3E-,Default%20layouts,-If%20you%27re%20using) and 
-  [Title & Meta](https://inertiajs.com/title-and-meta#:~:text=Server%2Dside%20rendering-,Title%20%26%20meta,-Since%20Inertia%20powered). It's 
+
+- I learned about [Default Layout](https://inertiajs.com/pages#default-layouts:~:text=%3C/script%3E-,Default%20layouts,-If%20you%27re%20using) and
+  [Title & Meta](https://inertiajs.com/title-and-meta#:~:text=Server%2Dside%20rendering-,Title%20%26%20meta,-Since%20Inertia%20powered). It's
   useful when you have a navbar.
 - Preserve Scroll
-- Tighten/Ziggy 
+- Tighten/Ziggy
 - Scroll Management
+- Lodash
+- I learned how to use Conditional Clauses, the when() method that allows me to conditionally apply query constraint.
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
 use App\Http\Controllers\UserController;
-use App\Models\User;
 use Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
-    Route::inertia('/', 'Home', ['users' => User::paginate(3)])->name('dashboard');
     Route::get('/profile', function () {
         return inertia('Profile', [
             'isActive' => 'profile',
@@ -14,6 +12,8 @@ Route::middleware('auth')->group(function () {
     })->name('profile');
     Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 });
+
+Route::get('/', [UserController::class, 'index'])->name('dashboard');
 
 Route::resource('users', UserController::class)->middleware([HandlePrecognitiveRequests::class]);
 Route::inertia('/register', 'Auth/Register')->name('register');
